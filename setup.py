5666d102b8fc9e5cb107e114fb666f65c6c3026e
SpeakinTelnet <gui.lac@protonmail.com>
2022-07-03 15:30:35 -0400

    Beta version bump

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/SpeakinTelnet/blockscan-python',
-    version='0.0.1',
+    version='1.1.0',
     zip_safe=False,
 )
