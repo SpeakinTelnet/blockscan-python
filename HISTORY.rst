b5411ac1e5de8a9f1f947822a888eb1e02dc90d2
SpeakinTelnet <gui.lac@protonmail.com>
2022-07-03 15:44:35 -0400

    Bump to beta version

--- HISTORY.rst
+++ HISTORY.rst
@@ -6,3 +6,9 @@ History
 ------------------
 
 * First release on PyPI.
+
+1.1.0 (2022-07-03)
+------------------
+
+* CI tests and deployment added
+* Packaging build tested and beta ready
--- blockscan/__init__.py
+++ blockscan/__init__.py
@@ -2,6 +2,6 @@ from blockscan.main import Blockscan
 from blockscan.connection.sync_connection import SyncConnection
 from blockscan.connection.async_connection import AsyncConnection
 
-__version__ = "0.0.1"
+__version__ = "1.1.0"
 
 __all__ = ["__version__", "Blockscan", "SyncConnection", "AsyncConnection"]
